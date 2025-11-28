ac1dbd9723a672ade61268d2f8900663809522ff
jamesbruten <enquiries@metoffice.gov.uk>
2025-11-28 10:45:34 +0000

    ticket:976 : jamescunningham-smith : '''Further Commit''' Core ticket:4612: Algorithm for flexible partitions
    git-svn-id: https://metomi/svn/lfric_apps.xm/main/trunk@15830 3896877f-8c05-4358-bc88-7947bb4308c7

--- dependencies.sh
+++ dependencies.sh
@@ -21,8 +21,8 @@
 # dependent changes should be updated to the revision at which those
 # changes were committed to the project's trunk
 
-export lfric_core_rev=54088
-export lfric_core_sources=
+export lfric_core_rev=
+export lfric_core_sources=cazld00001l:/var/tmp/persistent/review_copies/lfric_parts/t4612_core
 export casim_rev=apps2.2
 export casim_sources=
 export jules_rev=31238
